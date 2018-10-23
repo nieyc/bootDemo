dfe4dab5ff6e74e144ad09ead8a5f93c5d916ed7
nieyc <nieyc@cssweb.sh.cn>
2018-10-23 13:22:06 +0800

    modify

--- src/main/java/com/github/nyc/bootDemo/Application.java
+++ src/main/java/com/github/nyc/bootDemo/Application.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-//@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
